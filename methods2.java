033aaa93f70667b53da8ec465ae2f0fdedb81c00
AltheaBriggs <a.briggs@sjasd.ca>
2016-12-15 10:49:18 -0600

    project6
    assigment6

--- methods2.java
+++ methods2.java
@@ -31,72 +31,72 @@ public class methods2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner userInput = new Scanner(System.in);
+	Scanner userInput = new Scanner(System.in);
 		
 		
-					double[] student1 = {67.0,57,23.245,89.876,85,90,57,67,73,93,};
+	double[] student1 = {67.0,57,23.245,89.876,85,90,57,67,73,93,};
 		 	  
-					double[] student2 = new double[10];  
+	double[] student2 = new double[10];  
 		 	  
 					  
-					double average1 = 0;  
-					double average2 = 0;  
+	double average1 = 0;  
+	double average2 = 0;  
 		 		  
-					System.out.println("Please enter ten marks to be averaged:");  
+	System.out.println("Please enter ten marks to be averaged:");  
 		 		  
-					for(int i = 0; i < student2.length; i++){  
-						System.out.print("Please enter a mark: ");  
-						student2[i] = userInput.nextDouble();
+	for(int i = 0; i < student2.length; i++){  
+			System.out.print("Please enter a mark: ");  
+			student2[i] = userInput.nextDouble();
 					
-					}  
+	}  
 		 	  
-					average1 = calcAverage(student1);
-					average2 = calcAverage(student2);
+	average1 = calcAverage(student1);
+	average2 = calcAverage(student2);
 					
-					System.out.println("Your average is " + average2 + "!");
+		System.out.println("Your average is " + average2 + "!");
 		 		  
-					average1 = Sum / student1.length;  
+		average1 = Sum / student1.length;  
 		 		  
-					if(average2 > average1){  
+		if(average2 > average1){  
 				  
-		 			System.out.println("Your average is greater than student 1's by " + (average2 - average1) + " points.");  
+		 System.out.println("Your average is greater than student 1's by " + (average2 - average1) + " points.");  
 					  
-					} else if(average2 < average1){  
+			} else if(average2 < average1){  
 		 			  
-		 			System.out.println("Your average is less than student 1's by " + (average1 - average2) + " points.");  
+		System.out.println("Your average is less than student 1's by " + (average1 - average2) + " points.");  
 				  
-					} else {  
+			} else {  
 		 		  
-						System.out.println("Your average is equal to student 1's.");  
+			System.out.println("Your average is equal to student 1's.");  
 		 		  
-					}  
+			}  
 					
-						userInput.close();
+			userInput.close();
 					 
 	}
-				    /**
-			        * calcAverage will calculate users average compared to the other students average
-			        * 
-			        * 
-			        * @param The students marks will be stored.
-			        * @return average will be sent back, and compared to the other students.
-			        */ 
-				public static double calcAverage( double[] marks ){
+	/**
+		* calcAverage will calculate users average compared to the other students average
+		* 
+		* 
+		* @param The students marks will be stored.
+		* @return average will be sent back, and compared to the other students.
+	 */ 
+	public static double calcAverage( double[] marks ){
 				
-					double Sum = 0;
-					double average = 0;
+		double Sum = 0;
+		double average = 0;
 					
-					for(int i = 0; i < marks.length; i++){  
-						Sum += marks[i];
+		for(int i = 0; i < marks.length; i++){  
+			Sum += marks[i];
 						
-					}
+			}
 					
-					average = Sum / marks.length;
-					return average;
+		average = Sum / marks.length;
+		return average;
 					
 					
 						
-				}
+		}
 		 	
 
 		
---
+++ project6PrimeNumbers.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+/**
+ * 
+ */
+/**
+ *
+ * Name:Althea Briggs 
+ * Teacher:Mr.Hardman
+ * Assignment #6, Program #
+ * Date Last Modified:Dec.15 2016
+ *
+ */
+/**
+/**
+ * @author A.briggs
+ *
+ */
+public class project6PrimeNumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner userInput = new Scanner(System.in);
+		
+		int userNumber;
+		boolean checkPrime;
+		
+		System.out.println("Pick a integer ");
+		userNumber = userInput.nextInt();
+		
+		checkPrime = isPrime(userNumber);
+
+	}
+	
+	public static boolean isPrime(int userNumber){
+		
+		for(int i = 2; prime && i <= Math.sqrt(userNumber); i++){
+
+			/**
+			 * Check whether the remainder of userNumber divided by i equals 0.
+			 * If so, prime = false;
+			 *
+			 */
+				userNumber = 0
+			
+			
+			
+
+		}
+
+
+	}
+	
+}
